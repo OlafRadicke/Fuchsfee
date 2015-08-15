79c1a3a0e68f723eb32c3a0c673cfee5b13555bc
Olaf Radicke <briefkasten@olaf-radicke.de>
2015-08-15 09:27:02 +0200

    refactoring.

--- fuchsfee.go
+++ fuchsfee.go
@@ -1,22 +1,24 @@
 package main
 
-import "fmt"
-import "net/http"
-import "io/ioutil"
-import "encoding/json"
-// import "bytes"
+import (
+    "bytes"
+    "fmt"
+    "net/http"
+    "io/ioutil"
+    "encoding/json"
+)
 
-func main() {
-    fmt.Printf("Hello, world.\n")
+// Do a couchdb request and get back blog article as json.
+func getLastBlogArticles( couchdb_host string, result_limit string)([]byte, int){
+    var string_buffer bytes.Buffer
+    string_buffer.WriteString("http://")
+    string_buffer.WriteString(couchdb_host)
+    string_buffer.WriteString(":5984/tuxerjoch/_design/blog_article/_view/all?descending=true&limit=")
+    string_buffer.WriteString(result_limit)
 
-    url := "http://127.0.0.1:5984/tuxerjoch/_design/blog_article/_view/all?descending=true&limit=10"
-    fmt.Println("URL:>", url)
+    fmt.Println("URL:  ", string_buffer.String())
 
-//     var jsonStr = []byte(`{"title":"Buy cheese and bread for breakfast."}`)
-//     req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
-//     req.Header.Set("X-Custom-Header", "myvalue")
-
-    req, err := http.NewRequest("GET", url, nil)
+    req, err := http.NewRequest("GET", string_buffer.String(), nil)
     req.Header.Set("Content-Type", "application/json")
 
     client := &http.Client{}
@@ -31,30 +33,41 @@ func main() {
     fmt.Println("response Headers:", resp.Header)
     body, _ := ioutil.ReadAll(resp.Body)
 //     fmt.Println("response Body:", string(body))
+    return body, 0
+}
 
-    // Converting
-    //     "rows"/"value"/"title"
-//     type ArticleList struct {
-//         Rows []interface{} `json:"rows"`
-//     }
+// Object to represent the value part in the json tree.
+type Article []struct {
+    Value map[string]interface{} `json:"value"`
+}
 
+// Object to represent the rows part in the json tree.
+type ArticleList struct {
+    Rows Article `json:"rows"`
+}
 
-    type Article []struct {
-        Value map[string]interface{} `json:"value"`
-    }
-
-    type ArticleList struct {
-        Rows Article `json:"rows"`
-    }
-
+// Converting json to go objecth
+// Object hierarchy: "rows"/"value"/"title"
+func json_to_object(json_body []byte)(*ArticleList){
     article_list := &ArticleList{}
-    if err := json.Unmarshal( body, &article_list ); err != nil {
+    if err := json.Unmarshal(json_body, &article_list); err != nil {
         panic(err)
     }
     fmt.Println("============================")
     fmt.Println(article_list.Rows[0])
     fmt.Println("============================\n")
     fmt.Println(article_list.Rows[0].Value["title"])
+    return article_list
+}
 
-//     fmt.Println(article.Value["title"])
+
+func main() {
+    fmt.Printf("Hello, world.\n")
+
+    body, err := getLastBlogArticles("127.0.0.1", "10")
+    fmt.Println("err: ", err)
+    article_list := json_to_object(body)
+    fmt.Println("============================\n")
+    fmt.Println(article_list.Rows[0].Value["title"])
+
 }
