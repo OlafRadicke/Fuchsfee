3d8583c428655f52b70a25fe22c04617b8699494
Olaf Radicke <briefkasten@olaf-radicke.de>
2015-08-16 09:59:22 +0200

    refactoring.

--- fuchsfee.go
+++ fuchsfee.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-    "time"
     "fmt"
     "net/http"
-    "github.com/gorilla/feeds"
     "./couchdbrest"
     "./jsonconvert"
+    "./feedfactory"
 )
 
 
@@ -29,49 +28,7 @@ func main() {
     fmt.Println("Anzahl Einträge:", len(article_list.Rows))
 
 
-
-/////////////////////////////////////////////////////////////////////////////
-
-    now := time.Now()
-    feed := &feeds.Feed{
-        Title:       "jmoiron.net blog",
-        Link:        &feeds.Link{Href: "http://jmoiron.net/blog"},
-        Description: "discussion about tech, footie, photos",
-        Author:      &feeds.Author{"Jason Moiron", "jmoiron@jmoiron.net"},
-        Created:     now,
-    }
-
-    feed.Items = []*feeds.Item{
-        &feeds.Item{
-            Title:       "Limiting Concurrency in Go",
-            Link:        &feeds.Link{Href: "http://jmoiron.net/blog/limiting-concurrency-in-go/"},
-            Description: "A discussion on controlled parallelism in golang",
-            Author:      &feeds.Author{"Jason Moiron", "jmoiron@jmoiron.net"},
-            Created:     now,
-        },
-        &feeds.Item{
-            Title:       "Logic-less Template Redux",
-            Link:        &feeds.Link{Href: "http://jmoiron.net/blog/logicless-template-redux/"},
-            Description: "More thoughts on logicless templates",
-            Created:     now,
-        },
-        &feeds.Item{
-            Title:       "Idiomatic Code Reuse in Go",
-            Link:        &feeds.Link{Href: "http://jmoiron.net/blog/idiomatic-code-reuse-in-go/"},
-            Description: "How to use interfaces <em>effectively</em>",
-            Created:     now,
-        },
-    }
-
-    atom, _ := feed.ToAtom()
-    rss, _ := feed.ToRss()
-
-    fmt.Println(atom, "\n", rss)
-
-    fmt.Println("============================\n")
-    fmt.Println("atom:", atom)
-////////////////////////////////////////////////////////////////////////////
-
+    feedfactory.GetAtom()
 
 
     http.HandleFunc("/", handler)
